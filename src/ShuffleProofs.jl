cd2b18515dc423f53ce7ecc1cd8655027335af40
Janis Erdmanis <janiserdmanis@protonmail.ch>
2024-03-24 02:23:55 +0200

    implementing digest for simulators

--- src/ShuffleProofs.jl
+++ src/ShuffleProofs.jl
@@ -7,18 +7,12 @@ function __init__()
 end
 
 include("utils.jl") # Common functions
-
 include("prover.jl") 
-
 include("parser.jl") 
 include("io.jl") # Some convinience methods
-
 include("verifier.jl") 
-
 include("decryption.jl") 
-
 include("braid.jl")
-
 include("store.jl")
 
 end # module
